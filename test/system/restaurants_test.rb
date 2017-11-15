4a0cf8112cb70ba4d56c85ec80714fe4ca5f65b3
Chris <cdunham1989@gmail.com>
2017-11-15 17:14:23 +0000

    database cleaner setup to ensure feature test for deleting restaurant data is correct, functionality for deleting databases added and feature test is complete

--- test/system/restaurants_test.rb
+++ test/system/restaurants_test.rb
@@ -1,44 +1,40 @@
 require "application_system_test_case"
+require "database_cleaner"
 
 class RestaurantsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit restaurants_url
-  #
-  #   assert_selector "h1", text: "Restaurants"
-  # end
 
   test "listing a new restaurant" do
     visit restaurants_path
-
     click_on "Add restaurant"
-
     fill_in "name", with: "Cafe Boomstyle"
     fill_in "description", with: "mmm mmmm"
-
     click_on "Save Restaurant"
-
     assert_text "Name: Cafe Boomstyle"
   end
 
   test "editing restaurants details" do
-
     visit restaurants_path
-
     click_on "Add restaurant"
-
     fill_in "name", with: "Cafe Boomstyle"
     fill_in "description", with: "mmm mmmm"
-
     click_on "Save Restaurant"
-
     click_on "Edit"
-
     fill_in "name", with: "Cafe face"
     fill_in "description", with: "oooo yeah"
-
     click_on "Update Restaurant"
-
     assert_text "Name: Cafe face"
     assert_text "Description: oooo yeah"
+  end
+
+  test "deleting restaurant details" do
+    visit restaurants_path
+    click_on "Add restaurant"
+    fill_in "name", with: "This should be deleted"
+    fill_in "description", with: "Nothing useful"
+    click_on "Save Restaurant"
+    click_on "Back"
+    click_on "Delete"
+    page.accept_alert
+    assert_no_text "This should be deleted"
   end
 end
