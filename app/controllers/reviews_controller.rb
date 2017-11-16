3269599f622ebc76dcb5e8820463636b9a7577a1
Ed Lowther <ed.lowther[at]gmail.com>
2017-11-16 10:51:29 +0000

    Fixed UI problems related to showing individual reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,18 +5,23 @@ class ReviewsController < ApplicationController
 
 def create
   @review = Review.new(review_params)
-  @restaurant = Restaurant.find(params[:restaurant_id])
+  @restaurant = find_restaurant
   @review.restaurant = @restaurant
   @review.save
   redirect_to restaurant_reviews_path(@restaurant)
 end
 
 def show
+  @restaurant = find_restaurant
   @review = Review.find(params[:id])
 end
 
 private
   def review_params
     params.require(:review) .permit(:rating, :description)
+  end
+
+  def find_restaurant
+    return Restaurant.find(params[:restaurant_id])
   end
 end
