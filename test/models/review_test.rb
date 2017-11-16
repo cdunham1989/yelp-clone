f5d322f0c42d8818418b399099f0d8f5b2e27d68
oliverpople <oliverpople@gmail.com>
2017-11-16 15:01:04 +0000

    feature tests for average rating passing

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -32,13 +32,4 @@ class ReviewTest < ActiveSupport::TestCase
     assert_not review.save
   end
 
-  test "should return an average rating" do
-    restaurant = Restaurant.new(name: "cafe burp", description: "airy")
-    restaurant.save
-    review = Review.new(rating: "3", description: "average")
-    review.save
-
-    assert reviews.index.average_rating.average_rating == 2
-  end
-
 end
